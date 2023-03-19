3ee94eb13301dfb760827bcd3366fed81f9d4b25
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-18 19:24:04 -0600

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [number for number in num_list if number % 2 == 0]
+    print(evens)
+    return evens
+
+# should return [2, 4, 0, 8, 8]
+return_evens([1, 2, 3, 4, 7, 0, 5, 8, 8])
 
 def make_exclamation(sentence_list):
-    pass
+    exclamationed = [sentence + "!" for sentence in sentence_list]
+    return exclamationed
